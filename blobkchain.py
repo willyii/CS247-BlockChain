fa9990b96e8e75f6e86bf9d571cf3be7305a6b4f
Zoe Shang <ClockOrange@users.noreply.github.com>
2020-05-28 01:33:21 -0700

    Create blobkchain.py
    Basic function demo for blockchain: adding new block and new node

---
+++ blobkchain.py
@@ -0,0 +1,47 @@
+# Blockchain class
+
+import hashlib
+import json
+from time import time
+from flask import Flask, jsonify, request
+import block
+import node
+from urllib.parse import urlparse
+
+class blockchain:
+    def __init__(self):
+        self.chains = []
+        self.nodes = set()
+        self.currentTrans = []
+
+
+    # add block to chain 
+    def addBlock(self,nonce, prev_hash, pubKey):
+        newBlock = block.newBlock(nonce,prev_hash,pubKey)
+        self.currentTrans = [] #reset current transactions
+        self.chain.append(newBlock)
+        return newBlock # return a Json data
+    
+    def addNode(self,id,address):
+        parsed_url = urlparse(address)
+        newNode = None
+        addSuccess  = False
+        if parsed_url.netloc:
+            address = parsed_url.netloc
+            addSuccess = True
+        elif parsed_url.path:
+            address = parsed_url.path
+            addSuccess = True
+        else:
+            raise ValueError('Invalid URL')
+        if(addSuccess):
+            newNode = node.newNode(id,address)
+            self.nodes.add(newNode)
+        return newNode
+        
+    
+
+
+
+
+
