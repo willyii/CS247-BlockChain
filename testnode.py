64d52c52381d617d645ecb55cfcbc5a70c1a300f
xinlong <623365323@qq.com>
2020-05-30 12:12:05 -0700

    update testnode

--- testnode.py
+++ testnode.py
@@ -4,7 +4,7 @@ from blockchain import BlockChain
 from node import Node
 
 
-def getTestBlock():
+def getTestChain():
     testblock = BlockChain()
     test_trans = Transaction(f="master", to="0", header ="test1",value= 5)
     testblock.addUnused(test_trans)
@@ -13,8 +13,13 @@ def getTestBlock():
     return testblock 
 
 if __name__ == "__main__":
+
     testnode = Node(address = "0", name="fuck", min_ind = 0)
-    testnode.BlockChain = getTestBlock()
+    testnode.BlockChain = getTestChain()
+    print(testnode.BlockChain.tojson())
+    print("Block Chain tojson test pass")
+
 
     send = testnode.sendCoin("1", 7)
     print(send.tojson())
+    print("Send Coint tojson test pass")
