65372a8671316ded0040ae950b913c6226f03dca
xinlong <623365323@qq.com>
2020-05-31 21:32:56 -0700

    s

--- testFlask.py
+++ testFlask.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, Response   
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello Fucking Bitcoin"
+    return Response("Hello Fucking Bitcoin")
 
 if __name__ == "__main__":
     app.run()
