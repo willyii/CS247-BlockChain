a6c70a97d01fc5370d55c27e9a19ec432cba933c
xinlong1996@gmail.com <623365323@qq.com>
2020-06-10 10:20:09 -0700

    remove useless file

--- report.py
+++
@@ -1,7 +0,0 @@
-from transaction import Transaction 
-from block import Block
-from blockchain import BlockChain
-from node import Node
-
-b = Transaction(f="xinlong",to="zhuocheng", inlist=[], outlist=[], header="Xinlong send 5 coins to zhuocheng", value =5)
-print(b.tojson())
